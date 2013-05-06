40ec785888e7d0f4b677415bdb6d686434cf341f
darcher <darcher@linkedin.com>
2013-05-05 18:47:28 -0700

    switch to creole and add page titles

--- fliki.py
+++ fliki.py
@@ -3,13 +3,15 @@ from os.path import basename, exists, splitext, join
 from time import time
 import re
 
+import git
+from creole import creole2html
 from flask import Flask, request, render_template, redirect, url_for
-from markdown import markdown
+
 
 INDEX_PAGE = "home"
 PAGES_DIRECTORY = "pages"
 
-RE_PAGE_NAME = re.compile("^[a-zA-Z0-9\-]+$")
+RE_PAGE_NAME = re.compile("^[_a-zA-Z0-9\-]+$")
 
 app = Flask(__name__)
 
@@ -25,7 +27,7 @@ def page_show(page_name):
     if _page_exists(page_name):
         text = _read_page(page_name)
         html = _convert_to_html(text)
-        return render_template("page_show.html", content=html, page_name=page_name)
+        return render_template("page_show.html", content=html, page_name=page_name, title=humanize(page_name).title())
     else:
         return redirect("/%s/edit" % page_name)
 
@@ -37,7 +39,7 @@ def page_edit(page_name):
             text = _read_page(page_name)
         else:
             text = "# %s\n\nThis page does not exist yet. Replace this content and click Save to create it." % humanize(page_name)
-        return render_template("page_edit.html", text=text, page_name=page_name)
+        return render_template("page_edit.html", text=text, page_name=page_name, title=humanize(page_name).title())
     else:
         text = request.form["text"]
         _write_page(page_name, text)
@@ -51,14 +53,14 @@ def page_list():
     
 def _page_name_to_filename(page_name):
     if RE_PAGE_NAME.match(page_name):
-        return join(PAGES_DIRECTORY, "%s.md" % page_name.lower())
+        return join(PAGES_DIRECTORY, "%s.creole" % page_name.lower())
     else:
         raise ValueError("Invalid page name %s." % page_name)
 
 @app.template_filter()
 def humanize(page_name):
     """Clean up the page name to a human-readable name."""
-    return page_name.replace("-", " ").capitalize()    
+    return page_name.replace("-", " ").capitalize()
     
 def _filename_to_page_name(fname):
     return splitext(basename(fname))[0]
@@ -75,11 +77,11 @@ def _write_page(page_name, text):
     open(fname, "w").write(text)
     
 def _list_pages():
-    pages_glob = join(PAGES_DIRECTORY, "*.md")
-    return [_filename_to_page_name(fname) for fname in glob(pages_glob)]
+    pages_glob = join(PAGES_DIRECTORY, "*.creole")
+    return [_filename_to_page_name(fname) for fname in sorted(glob(pages_glob))]
 
 def _convert_to_html(text):
-    return markdown(text)
+    return creole2html(unicode(text))
 
 if __name__=="__main__":
     app.run(debug=True)
